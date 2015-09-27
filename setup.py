1942038736440f69140e78ddf254d723e66d1c1b
lemmetry <artemkisel@yahoo.com>
2015-09-26 21:35:11 -0400

    Added setup.py for PyPI distribution.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='payeezy',
+    version='1.0.0a1',
+    description='Unofficial Python 3 module to process basic(purchase and authorize) transactions with Payeezy',
+    url='https://github.com/lemmetry/payeezy',
+    author='Artem Kisel',
+    author_email='artem.a.kisel@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Office/Business :: Financial :: Point-Of-Sale',
+    ],
+    keywords='Payeezy purchase authorize transaction FirstData',
+
+)
