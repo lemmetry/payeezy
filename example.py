bb160ce65978e5f7eeef6c6d6ea5fce69dfae5fe
lemmetry <artemkisel@yahoo.com>
2015-10-01 22:01:39 -0400

    Trying different approach - calling processing methods thru class.

--- example.py
+++ example.py
@@ -1,19 +1,21 @@
-from payeezy import payeezy
+from payeezy import transactions
+
+payeezy = transactions.Transaction
 
 # Declare your API_Key, API_SECRET and TOKEN
 payeezy.API_KEY = 'y6pWAJNyJyjGv66IsVuWnklkKUPFbb0a'
 payeezy.API_SECRET = '86fbae7030253af3cd15faef2a1f4b67353e41fb6799f576b5093ae52901e6f7'
 payeezy.TOKEN = 'fdoa-a480ce8951daa73262734cf102641994c1e55e7cdf4c02b6'
 
-purchase_result = payeezy.process_purchase(transaction_total='2499',
-                                           card_type='VISA',
-                                           card_number='4005519200000004',
-                                           card_expiry='1218',
-                                           card_cvv='123',
-                                           cardholder_name='Donald Duck',
-                                           merchant_reference='Sale')
-
-if payeezy.is_transaction_approved(purchase_result):
+transaction = payeezy()
+purchase_result = transaction.process_purchase(transaction_total='2499',
+                                               card_type='VISA',
+                                               card_number='4005519200000004',
+                                               card_expiry='1218',
+                                               card_cvv='123',
+                                               cardholder_name='Donald Duck',
+                                               merchant_reference='Sale')
+if transaction.is_transaction_approved():
     print('Thank you for your purchase')
 else:
-    print(payeezy.report_transaction_error_messages(purchase_result))
+    print(transaction.report_transaction_error_messages)
--- tests/test.py
+++ tests/test.py
@@ -1,36 +1,23 @@
 import unittest
-from payeezy import payeezy
+from payeezy import transactions
 
 
-payeezy.API_KEY = 'y6pWAJNyJyjGv66IsVuWnklkKUPFbb0a'
-payeezy.API_SECRET = '86fbae7030253af3cd15faef2a1f4b67353e41fb6799f576b5093ae52901e6f7'
+payeezy = transactions.Transaction
 payeezy.TOKEN = 'fdoa-a480ce8951daa73262734cf102641994c1e55e7cdf4c02b6'
+payeezy.API_SECRET = '86fbae7030253af3cd15faef2a1f4b67353e41fb6799f576b5093ae52901e6f7'
+payeezy.API_KEY = 'y6pWAJNyJyjGv66IsVuWnklkKUPFbb0a'
 
 
-class TestContainer(unittest.TestCase):
-    pass
+class OverallTestCase(unittest.TestCase):
+    def test_purchase(self):
+        transaction = payeezy()
+        transaction.process_purchase('2499', 'visa', '4005519200000004', '1019', '123', 'Donald Duck')
+        self.assertTrue(transaction.is_transaction_approved())
 
-
-def card_type_and_number_test_generator(card_type, card_number):
-    def test(self):
-        purchase_result = payeezy.process_purchase(transaction_total='2499',
-                                                   card_type=card_type,
-                                                   card_number=card_number,
-                                                   card_expiry='1218',
-                                                   card_cvv='123',
-                                                   cardholder_name='Donald Duck',
-                                                   merchant_reference='Sale')
-        self.assertTrue(payeezy.is_transaction_approved(purchase_result))
-    return test
+    def test_authorization(self):
+        transaction = payeezy()
+        transaction.process_authorization('2499', 'visa', '4005519200000004', '1019', '123', 'Donald Duck')
+        self.assertTrue(transaction.is_transaction_approved())
 
 if __name__ == '__main__':
-    cards = [
-        ['visa', '4005519200000004'],
-        ['visa', '4012000033330026']
-    ]
-
-    for t in cards:
-        test_name = 'test_%s_%s' % (t[0][:2], t[1][-4:])
-        test = card_type_and_number_test_generator(t[0], t[1])
-        setattr(TestContainer, test_name, test)
     unittest.main()
