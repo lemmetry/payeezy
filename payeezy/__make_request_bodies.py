ff6b2228253e97fd542eaf16026debc5d04ebaf3
Artem Kisel <artemkisel@yahoo.com>
2019-09-22 22:04:40 -0400

    Added billing_address_*** parameters to the request body.

--- payeezy/__make_request_bodies.py
+++ payeezy/__make_request_bodies.py
@@ -6,7 +6,22 @@ def make_request_body_for_purchase(transaction_total,
                                    card_expiry,
                                    card_cvv,
                                    cardholder_name,
-                                   merchant_reference=''):
+                                   merchant_reference='',
+                                   billing_address_street='',
+                                   billing_address_city='',
+                                   billing_address_state_province='',
+                                   billing_address_country='',
+                                   billing_address_zip_postal_code='',
+                                   billing_address_email='',
+                                   billing_address_phone_type='',
+                                   billing_address_phone_number=''):
+
+    transaction_total = str(transaction_total)
+    card_type = str(card_type)
+    card_number = str(card_number)
+    card_expiry = str(card_expiry)
+    card_cvv = str(card_cvv)
+    cardholder_name = str(cardholder_name)
 
     request_body = {
         "merchant_ref": merchant_reference,
@@ -21,6 +36,18 @@ def make_request_body_for_purchase(transaction_total,
             "card_number": card_number,
             "exp_date": card_expiry,
             "cvv": card_cvv
+        },
+        "billing_address": {
+            "city": billing_address_city,
+            "country": billing_address_country,
+            "email": billing_address_email,
+            "phone": {
+                "type": billing_address_phone_type,
+                "number": billing_address_phone_number
+            },
+            "street": billing_address_street,
+            "state_province": billing_address_state_province,
+            "zip_postal_code": billing_address_zip_postal_code
         }
     }
 
--- payeezy/api.py
+++ payeezy/api.py
@@ -47,11 +47,28 @@ class API(object):
                          card_expiry,
                          card_cvv,
                          cardholder_name,
-                         merchant_reference=''):
+                         merchant_reference='',
+                         billing_address_street='',
+                         billing_address_city='',
+                         billing_address_state_province='',
+                         billing_address_country='',
+                         billing_address_zip_postal_code='',
+                         billing_address_email='',
+                         billing_address_phone_type='',
+                         billing_address_phone_number=''):
+        # billing_address* parameters are optional for Payeezy API, however merchants who have turned on AVS check,
+        # would require to provide "billing_address" object in the request payload.
 
         request_body = make_request_body_for_purchase(transaction_total, card_type, card_number, card_expiry, card_cvv,
-                                                      cardholder_name, merchant_reference)
+                                                      cardholder_name, merchant_reference, billing_address_street,
+                                                      billing_address_city, billing_address_state_province,
+                                                      billing_address_country, billing_address_zip_postal_code,
+                                                      billing_address_email, billing_address_phone_type,
+                                                      billing_address_phone_number)
         payload = json.dumps(request_body)
+        # According to Payeezy Sandbox, response would contain "avs & token object" fields only if the merchant has
+        # enabled AVS check and Token based services with Payeezy.
+        # TODO How do we test it?
 
         purchase = transactions.Transaction(self.api_key, self.api_secret, self.token, self.url, payload)
         purchase.run_transaction()
